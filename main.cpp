60ad88c1760ca2ff659ec7999d93480246e32afa
Nikita Zhernovenkov <nikzhernovenkov778@gmail.com>
2020-12-29 01:57:26 +0300

    Add Infinite cycle and exit

--- main.cpp
+++ main.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
 #include "Funcs.h"
 using namespace std;
-
+void EnterValues(float& a, float& b)
+{
+	cout << "Введите операнд А:";
+	cin >> a;
+	cout << "Введите операнд B:";
+	cin >> b;
+}
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	int choice;
 	float a, b;
-	cout << "Введите операнд А:";
-	cin >> a;
-	cout << "Введите операнд B:";
-	cin >> b;
-	cout << "1)Сумма"<<endl;
-	cout << "2)Умножение" << endl;
-	cout << "3)Вычитание" << endl;
-	cout << "4)Деление" << endl;
-	cout << "Введите номер операции: ";
-	cin >> choice;
-	switch (choice)
+	while (true)
 	{
-	case 1:
-		cout << Sum(a, b) << endl;
-		break;
-	case 2:
-		cout << Mul(a, b) << endl;
-		break;
-	case 3:
-		cout << Sub(a, b) << endl;
-		break;
-	case 4:
-		cout << Div(a, b) << endl;
-		break;
-	case 5:
-		break;
-	default:
-		cout << "Неверная операция";
-		break;
+		cout << "1)Сумма" << endl;
+		cout << "2)Умножение" << endl;
+		cout << "3)Вычитание" << endl;
+		cout << "4)Деление" << endl;
+		cout << "5)Выход" << endl;
+		cout << "Введите номер операции: ";
+		cin >> choice;
+		if (choice == 5)
+		{
+			break;
+		}
+		switch (choice)
+		{
+		case 1:
+			EnterValues(a, b);
+			cout <<"Ответ: " << Sum(a, b) << endl << endl;
+			break;
+		case 2:
+			EnterValues(a, b);
+			cout << "Ответ: " << Mul(a, b) << endl << endl;
+			break;
+		case 3:
+			EnterValues(a, b);
+			cout << "Ответ: " << Sub(a, b) << endl << endl;
+			break;
+		case 4:
+			EnterValues(a, b);
+			cout << "Ответ: " << Div(a, b) << endl << endl;
+			break;
+		default:
+			cout << "Неверная операция"<<endl;
+			break;
+		}
 	}
-	system("pause");
 }
