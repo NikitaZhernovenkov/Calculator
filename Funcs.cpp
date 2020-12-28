6d2e5c5e6f7ef30a7d01f8e25fd46cf9380a5be8
Nikita Zhernovenkov <nikzhernovenkov778@gmail.com>
2020-12-29 00:48:19 +0300

    Add Mul function

--- Funcs.cpp
+++ Funcs.cpp
@@ -4,3 +4,7 @@ double Sum(double a, double b)
 {
 	return a + b;
 }
+double Mul(double a, double b)
+{
+	return a * b;
+}
--- main.cpp
+++ main.cpp
@@ -5,4 +5,5 @@ using namespace std;
 int main()
 {
 	cout << Sum(10.5, 6.2) << endl;
+	cout << Mul(10.5, 6.2) << endl;
 }
