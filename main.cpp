60e4c1647e6c37f32da5dfd8ab25018b0e93d787
Nikita Zhernovenkov <nikzhernovenkov778@gmail.com>
2020-12-29 01:39:09 +0300

    Creating switch menu and entering values

--- main.cpp
+++ main.cpp
@@ -4,8 +4,38 @@ using namespace std;
 
 int main()
 {
-	cout << Sum(10.5, 6.2) << endl;
-	cout << Mul(10.5, 6.2) << endl;
-	cout << Sub(10.5, 6.1) << endl;
-	cout << Div(10.5, 6.1) << endl;
+	setlocale(LC_ALL, "rus");
+	int choice;
+	float a, b;
+	cout << "Введите операнд А:";
+	cin >> a;
+	cout << "Введите операнд B:";
+	cin >> b;
+	cout << "1)Сумма"<<endl;
+	cout << "2)Умножение" << endl;
+	cout << "3)Вычитание" << endl;
+	cout << "4)Деление" << endl;
+	cout << "Введите номер операции: ";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << Sum(a, b) << endl;
+		break;
+	case 2:
+		cout << Mul(a, b) << endl;
+		break;
+	case 3:
+		cout << Sub(a, b) << endl;
+		break;
+	case 4:
+		cout << Div(a, b) << endl;
+		break;
+	case 5:
+		break;
+	default:
+		cout << "Неверная операция";
+		break;
+	}
+	system("pause");
 }
